a6587b85f314de48cf76ef8f23e8d5bb27079e73
Preetham Angya <preetham.angya@happiestminds.com>
2016-09-19 16:29:57 +0530

    make things better

--- src/main/java/com/mentorMentee/interfaceProvider/Provider.java
+++ src/main/java/com/mentorMentee/interfaceProvider/Provider.java
@@ -1,14 +1,14 @@
 package com.mentorMentee.interfaceProvider;
 
 public interface Provider {
-	String DRIVER="org.postgresql.Driver"; 
-	String CONNECTION_URL="jdbc:postgresql://localhost:5432/MentorMentee";  
-	String USERNAME="postgres";  
-	String PASSWORD="preeth";
+	//String DRIVER="org.postgresql.Driver"; 
+	//String CONNECTION_URL="jdbc:postgresql://localhost:5432/MentorMentee";  
+	//String USERNAME="postgres";  
+	//String PASSWORD="preeth";
 	
-	//String DRIVER="org.postgresql.Driver";  
-	//String CONNECTION_URL="jdbc:postgresql://ec2-54-163-248-218.compute-1.amazonaws.com:5432/d6qcrbei5v8a52";  
-	//String USERNAME="meyfsvbnxvebgo";  
-	//String PASSWORD="FehZNUZ-n8xNnIflCfeCv5JKhC";
+	String DRIVER="org.postgresql.Driver";  
+	String CONNECTION_URL="jdbc:postgresql://ec2-54-163-248-218.compute-1.amazonaws.com:5432/d6qcrbei5v8a52";  
+	String USERNAME="meyfsvbnxvebgo";  
+	String PASSWORD="FehZNUZ-n8xNnIflCfeCv5JKhC";
 	
 }
